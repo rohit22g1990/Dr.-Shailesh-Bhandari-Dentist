4b665be4fd52ef2f32e13093143b0895d88a0352
rohit22g1990 <rohitghotkar123@gmail.com>
2021-05-02 01:11:40 +0530

    Gallery images changes

--- common.php
+++ common.php
@@ -164,19 +164,19 @@ class Common {
         return [
             '1.jpg',
             '2.jpg',
-            '3.jpg',
+            // '3.jpg',
             '4.jpg',
-            '5.jpg',
+            // '5.jpg',
             '6.jpg',
             '7.jpg',
             // 'assets/images/gallery/8.jpg',
             '9.jpg',
-            '10.jpg',
+            // '10.jpg',
             '11.jpg',
             '12.jpg',
             '13.jpg',
             '14.jpg',
-            '15.jpg',
+            // '15.jpg',
             '16.jpg',
             '17.jpg'
         ];
